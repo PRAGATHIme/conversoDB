2d6fe7313fcf00a499340fd3e417b63ff81af9b5
PRAGATHIme <pragathi4304@gmail.com>
2024-11-30 18:10:35 +0530

    added profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,8 @@ import Navbar from "./Navbar";
 import "./Profile.css";
 const Profile = () => {
   return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
+    <div style={{ textAlign: "center", padding: "120px" }}>
+      <Navbar />
       <h2>Enterprise name</h2>
       <p>Enterprise Owner</p>
       <p>Contact</p>
